6f0a68f6de8a91280afd720852688d62e8f215c6
Yutaka Nishimura <ytk.nishimura@gmail.com>
2020-06-26 02:46:26 +0900

    support nonce

--- src/BlockChain.hs
+++ src/BlockChain.hs
@@ -4,6 +4,7 @@ module BlockChain where
 
 import           Data.Time.Calendar
 import           Data.Time.Clock
+import           Debug.Trace
 import           Utils
 
 type Trans = BlockChain
@@ -26,7 +27,7 @@ defaultBlockChain now = BlockChain {_pool = [], _chain = [defaultBlock now]}
 
 defaultBlock now = Block now [] 0 (sha "")
 
-updateChain bc now nonce =
+updateChain bc now =
   bc
     { _pool = []
     , _chain =
@@ -39,8 +40,33 @@ updateChain bc now nonce =
             }
         ]
     }
+  where
+    nonce = proofOfWork bc now
 
-hashBlock block = sha . show $ block
+hashBlock = sha . show
+
+difficulty = 2
+
+solvNonce :: BlockChain -> Bool -> Integer
+solvNonce block True = _nonce block - 1
+solvNonce block False =
+  solvNonce
+    block {_nonce = _nonce block + 1}
+    (all (uncurry (==)) $ zip (take difficulty (hashBlock block)) (repeat '0'))
+
+proofOfWork bc now = solvNonce block False
+  where
+    trs = _pool bc
+    prev = last (_chain bc)
+    prev_hash = hashBlock prev
+    nonce = 0
+    block =
+      Block
+        { _ts = _ts prev
+        , _transactions = trs
+        , _nonce = nonce
+        , _prev_hash = prev_hash
+        }
 
 addTrans bc trans = bc {_pool = _pool bc ++ [trans]}
 
@@ -48,11 +74,11 @@ pp (BlockChain _ c) =
   putStrLn $
   concatMap
     (\(i, xs) ->
-       take 25 (repeat '=') ++
+       replicate 25 '=' ++
        " Chain " ++
        show i ++
        " " ++
-       take 25 (repeat '=') ++
+       replicate 25 '=' ++
        "\n" ++
        map
          (\case
@@ -64,20 +90,18 @@ pp (BlockChain _ c) =
             c -> c)
          (show xs))
     (zip [0 ..] c) ++
-  take 20 (repeat '*')
+  replicate 20 '*'
 
 doMain = do
-  putStrLn $ sha "test"
   d1 <- getCurrentTime
   let blockChain1 = defaultBlockChain d1
   let blockChain1a = addTrans blockChain1 (Transaction "A" "B" 1.0)
   pp blockChain1a
-  let prev_hash1 = hashBlock $ last (_chain blockChain1a)
   d2 <- getCurrentTime
-  let blockChain2 = updateChain blockChain1a d2 3
+  let blockChain2 = updateChain blockChain1a d2
   let blockChain2a = addTrans blockChain2 (Transaction "C" "D" 2.0)
   let blockChain2b = addTrans blockChain2a (Transaction "X" "Y" 3.0)
   pp blockChain2b
   d3 <- getCurrentTime
-  let blockChain3 = updateChain blockChain2b d3 5
+  let blockChain3 = updateChain blockChain2b d3
   pp blockChain3
